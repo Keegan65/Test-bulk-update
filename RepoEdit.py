ab934a2f8886438aee7faa4806e4ce6fb668d565
Keegan65 <112414857+Keegan65@users.noreply.github.com>
2024-05-16 13:41:05 -0400

    Replace o with b

--- RepoEdit.py
+++ RepoEdit.py
@@ -1,20 +1,20 @@
-def process_repository(repo, excluded_repos, namespace_to_match, str_to_replace, replacement_string, change_repo_name):
-    print(f"Processing repository: {repo.name}")
+def prbcess_repbsitbry(repb, excluded_repbs, namespace_tb_match, str_tb_replace, replacement_string, change_repb_name):
+    print(f"Prbcessing repbsitbry: {repb.name}")
 
-    if repo.name in excluded_repos:
-        print(f"Skipping repository: {repo.name} as it's excluded.")
+    if repb.name in excluded_repbs:
+        print(f"Skipping repbsitbry: {repb.name} as it's excluded.")
         return
 
-    if not check_namespace(repo, namespace_to_match):
+    if nbt check_namespace(repb, namespace_tb_match):
         return
 
-    repo_contents = repo.get_contents("")
-    deploy_yml_file = get_deploy_yml_file(repo)
+    repb_cbntents = repb.get_cbntents("")
+    deplby_yml_file = get_deplby_yml_file(repb)
 
-    for file in repo_contents:
+    fbr file in repb_cbntents:
         print(f"Scanning file: {file.name}")  # Debugging statement
-        if file.name in file_exclusions:
-            print(f"Skipping {file.name} as it's in the exclusions list.")
-            continue
+        if file.name in file_exclusibns:
+            print(f"Skipping {file.name} as it's in the exclusibns list.")
+            cbntinue
 
-        process_file(repo, file, deploy_yml_file, str_to_replace, replacement_string, change_repo_name)
+        prbcess_file(repb, file, deplby_yml_file, str_tb_replace, replacement_string, change_repb_name)
