5da9dd4e54761256f6fa40e250336354d4c721f3
Keegan65 <112414857+Keegan65@users.noreply.github.com>
2024-05-16 13:17:58 -0400

    Update RepoEdit.py

--- RepoEdit.py
+++ RepoEdit.py
@@ -1,10 +1,4 @@
-from github import Github
-import os
-import yaml
-
-
-def process_repository(repo, excluded_repos, namespace_to_match, repos_to_change, file_exclusions, str_to_replace,
-                       replacement_string, change_repo_name):
+def process_repository(repo, excluded_repos, namespace_to_match, str_to_replace, replacement_string, change_repo_name):
     print(f"Processing repository: {repo.name}")
 
     if repo.name in excluded_repos:
@@ -18,87 +12,9 @@ def process_repository(repo, excluded_repos, namespace_to_match, repos_to_change
     deploy_yml_file = get_deploy_yml_file(repo)
 
     for file in repo_contents:
+        print(f"Scanning file: {file.name}")  # Debugging statement
         if file.name in file_exclusions:
             print(f"Skipping {file.name} as it's in the exclusions list.")
             continue
 
         process_file(repo, file, deploy_yml_file, str_to_replace, replacement_string, change_repo_name)
-
-
-def check_namespace(repo, namespace_to_match):
-    if not namespace_to_match:
-        return True
-
-    deploy_yml_content = get_deploy_yml_content(repo)
-    argo_app = deploy_yml_content.get("jobs", {}).get("Deploy-To-GKE", {}).get("with", {}).get("ARGO_APP")
-
-    if argo_app not in namespace_to_match:
-        print(f"Namespace '{argo_app}' does not match, moving to the next repository.")
-        return False
-
-    return True
-
-
-def get_deploy_yml_file(repo):
-    deploy_yml_path = ".github/workflows/Deploy.yml"
-    try:
-        return repo.get_contents(deploy_yml_path)
-    except Exception as e:
-        print(f"No Deploy.yml found in the repository: {e}")
-        return None
-
-
-def get_deploy_yml_content(repo):
-    deploy_yml_file = get_deploy_yml_file(repo)
-    if deploy_yml_file:
-        return yaml.safe_load(deploy_yml_file.decoded_content)
-    else:
-        return {}
-
-
-def process_file(repo, file, deploy_yml_file, str_to_replace, replacement_string, change_repo_name):
-    print(f"Processing file: {file.name}")
-    try:
-        file_content = repo.get_contents(file.path).decoded_content.decode()
-        if str_to_replace in file_content:
-            new_file_content = file_content.replace(str_to_replace, replacement_string)
-            repo.update_file(file.path, f"Replace {str_to_replace} with {replacement_string}", new_file_content,
-                             file.sha)
-            print(f"Replaced in {file.name}")
-
-            if change_repo_name and str_to_replace in repo.name:
-                new_repo_name = repo.name.replace(str_to_replace, replacement_string)
-                repo.edit(name=new_repo_name)
-                print(f"Repository name changed to: {new_repo_name}")
-        else:
-            print(f"The string {str_to_replace} is not found in {file.name}")
-    except Exception as e:
-        print(f"An error occurred while processing {file.name}: {e}")
-
-
-def main():
-    str_to_replace = os.getenv('STR_TO_REPLACE')
-    replacement_string = os.getenv('REPLACEMENT_STRING')
-    repos_to_change = os.environ.get('REPOS_TO_CHANGE', '').split(',')
-    excluded_repos = os.environ.get('EXCLUDED_REPOS', '').split(',')
-    namespace_to_match = os.environ.get('NAME_SPACE', '').split(',')
-    file_exclusions = os.environ.get('FILE_EXCLUSIONS', '').split(',')
-    change_repo_name = os.getenv('CHANGE_REPO_NAME', 'true').lower() == 'true'
-    access_token = os.getenv('GITHUB_TOKEN')
-
-    g = Github(access_token)
-
-    if not repos_to_change:
-        print("No repositories specified to change. Processing all repositories owned by the user.")
-        for repo in g.get_user().get_repos(type="owner"):
-            process_repository(repo, excluded_repos, namespace_to_match, repos_to_change, file_exclusions,
-                               str_to_replace, replacement_string, change_repo_name)
-    else:
-        for repo_name in repos_to_change:
-            repo = g.get_user().get_repo(repo_name)
-            process_repository(repo, excluded_repos, namespace_to_match, repos_to_change, file_exclusions,
-                               str_to_replace, replacement_string, change_repo_name)
-
-
-if __name__ == "__main__":
-    main()
